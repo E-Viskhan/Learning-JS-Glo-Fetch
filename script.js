d0a2e7411d5ee40ce747aba38e206e471e04ffd7
E-Viskhan <viskhan11@gmail.com>
2022-01-06 07:34:16 +0300

    Реализация получения и отправки данных через XMLHttpRequest

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
-const getData = ({ url, method = 'GET' }) => fetch(url, { method });
+const getData = ({ url, method = 'GET' }) => {
+  let xhr = new XMLHttpRequest();
 
-const sendData = ({ url, method = 'POST', data = {} }) => fetch(url, { method, body: data });
+  xhr.open(method, url);
+  xhr.send();
 
-getData({ url: 'db.json' })
-  .then(data => data.json())
-  .then(json => {
-    console.log(json);
-    sendData({ url: 'https://jsonplaceholder.typicode.com/posts', data: JSON.stringify(json) })
-      .then(() => {
-        console.log('Успешная отправка данных');
-      })
-      .catch(() => console.error(new Error('Что-то пошло не так при отправке данных')));
-  })
-  .catch(() => {
-    console.error(new Error(`Ошибка при получении файла. Возможна ошибка в названии файла`));
+  return xhr;
+};
+
+const sendData = ({ url, method = 'POST', data = {} }) => {
+  let xhr = new XMLHttpRequest();
+
+  xhr.open(method, url);
+  xhr.send(data);
+
+  return xhr;
+};
+
+let response = getData({ url: 'db.json' });
+let data;
+
+response.onload = () => {
+  if (response.status != 200) {
+    console.error(new Error(`Ошибка ${response.status}: ${response.statusText}`));
+    return;
+  }
+
+  data = JSON.parse(response.response);
+  console.log('Полученные данные из db.json:');
+  console.log(data);
+
+  let sendResponse = sendData({
+    url: 'https://jsonplaceholder.typicode.com/posts',
+    data: JSON.stringify(data)
   });
+
+  sendResponse.onload = () => {
+    if (sendResponse.status != 201) {
+      console.error(new Error(`Ошибка ${response.status}: ${response.statusText}`));
+      return;
+    }
+
+    let answer = JSON.parse(sendResponse.response);
+    console.log('Полученные ответ из jsonplaceholder:');
+    console.log(answer);
+  };
+
+};
